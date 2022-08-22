027d451b577ba07baff2f2f92483c22c6187a0a2
Hasan-Mac <hasan@klapify.com>
2022-08-23 00:47:51 +0600

    first commit

---
+++ Furniture_app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Furniture_app
+//
+//  Created by Abu Anwar MD Abdullah on 14/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            HomeScreen()
+        }
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
